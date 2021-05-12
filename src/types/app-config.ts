85d48542dff0075640b3c785064639f54ba63e73
Alexander Yakhatin <sasha.yahatin@yandex.ru>
2021-05-12 20:36:39 +0300

    updd ts-types of app-config and order

--- src/types/app-config.ts
+++ src/types/app-config.ts
@@ -1,4 +1,9 @@
 export interface AppConfigInterface {
     config_id: number;
     ym_counterID: number | null;
+    contacts_phone: string | null;
+    contacts_email: string | null;
+    contacts_instagram: string | null;
+    contacts_wa: string | null;
+    contacts_city: string | null;
 }
--- src/types/order.ts
+++ src/types/order.ts
@@ -1,6 +1,8 @@
 export interface OrderClientDataRequestInterface {
     order_client_name: string;
     order_client_phone: string;
+    order_client_email?: string;
+    order_client_message?: string;
 }
 
 export interface OrderRequestInterface extends OrderClientDataRequestInterface {
@@ -12,6 +14,8 @@ export interface OrderDataInterface {
     cart_id?: string;
     order_client_name: string;
     order_client_phone: string;
+    order_client_email: string | null;
+    order_client_message: string | null;
     order_date: string;
     order_modified: string;
 }
